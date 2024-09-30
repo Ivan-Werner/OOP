d44d1c527551d3aaf92e88065bc9c708529bc4c6
Ivan_K <ikalinov1989yandex.ru>
2024-09-30 20:07:34 +0400

    Homework 14.2 for checking

--- src/product.py
+++ src/product.py
@@ -1,5 +1,6 @@
 
 class Product:
+
     name: str
     description: str
     price: float
@@ -8,13 +9,43 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
 
-if __name__ == '__main__':
-    product = Product("Smartphone", "Phone", 100.50, 2)
-    print(product.name)
-    print(product.description)
-    print(product.price)
-    print(product.quantity)
+    @classmethod
+    def new_product(cls, product_dict: dict):
+        return cls(**product_dict)
+
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = new_price
+
+
+
+# if __name__ == '__main__':
+#     product = Product("Smartphone", "Phone", 100.50, 2)
+#     print(product.name)
+#     print(product.description)
+#     print(product.price)
+#     print(product.quantity)
+#
+#     product2 = Product.new_product({"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
+#          "quantity": 5})
+#     print(product2.name)
+#     print(product2.description)
+#     print(product2.price)
+#     print(product2.quantity)
+#
+#     product2.price = -100
+#     print(product2.price)
+#     product2.price = 100
+#     print(product2.price)
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,24 @@
+from src.product import Product
+
+from src.product import Product
+
+
 def test_product_init(product):
     assert product.name == "Iphone 15"
     assert product.description == "512GB, Gray space"
     assert product.price == 210000.0
     assert product.quantity == 8
+
+
+def test_product_added():
+    product = Product("Smartphone", "Phone", 100.50, 2)
+    product.name = "Smartphone"
+    product.description = "Phone"
+    product.price = 100.50
+    product.quantity = 2
+
+
+def test_product_update(capsys, product):
+    product.price = -100
+    message = capsys.readouterr()
+    assert message.out.strip() == "Цена не должна быть нулевая или отрицательная"
